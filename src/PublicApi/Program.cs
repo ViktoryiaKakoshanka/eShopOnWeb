09febfc1c5550396c7a11513d342e0fc4a572147
Viktoryia Kakoshanka <Viktoryia.Kakoshanka@lseg.com>
2024-11-27 09:27:27 +0100

    UseSwaggerUI API

--- src/PublicApi/Program.cs
+++ src/PublicApi/Program.cs
@@ -167,10 +167,17 @@ app.UseSwagger();
 
 // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
 // specifying the Swagger JSON endpoint.
-app.UseSwaggerUI(c =>
+if (app.Environment.IsDevelopment())
 {
-    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-});
+    app.UseSwaggerUI();
+}
+else
+{
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    });
+}
 
 app.MapControllers();
 app.MapEndpoints();
