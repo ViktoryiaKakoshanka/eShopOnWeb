6d3cf488c41670a4808cd03630180f4d864942e2
Viktoryia Kakoshanka <Viktoryia.Kakoshanka@lseg.com>
2024-12-09 00:18:15 +0100

    key vault

--- src/ApplicationCore/Services/OrderService.cs
+++ src/ApplicationCore/Services/OrderService.cs
@@ -1,4 +1,6 @@
 ﻿using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
 using System.Threading.Tasks;
 using Ardalis.GuardClauses;
 using Microsoft.eShopWeb.ApplicationCore.Entities;
@@ -6,6 +8,7 @@ using Microsoft.eShopWeb.ApplicationCore.Entities.BasketAggregate;
 using Microsoft.eShopWeb.ApplicationCore.Entities.OrderAggregate;
 using Microsoft.eShopWeb.ApplicationCore.Interfaces;
 using Microsoft.eShopWeb.ApplicationCore.Specifications;
+using Microsoft.Extensions.Logging;
 
 namespace Microsoft.eShopWeb.ApplicationCore.Services;
 
@@ -15,16 +18,19 @@ public class OrderService : IOrderService
     private readonly IUriComposer _uriComposer;
     private readonly IRepository<Basket> _basketRepository;
     private readonly IRepository<CatalogItem> _itemRepository;
+    private readonly ILogger<OrderService> _logger;
 
     public OrderService(IRepository<Basket> basketRepository,
         IRepository<CatalogItem> itemRepository,
         IRepository<Order> orderRepository,
-        IUriComposer uriComposer)
+        IUriComposer uriComposer,
+        ILogger<OrderService> logger)
     {
         _orderRepository = orderRepository;
         _uriComposer = uriComposer;
         _basketRepository = basketRepository;
         _itemRepository = itemRepository;
+        _logger = logger;
     }
 
     public async Task CreateOrderAsync(int basketId, Address shippingAddress)
@@ -49,5 +55,25 @@ public class OrderService : IOrderService
         var order = new Order(basket.BuyerId, shippingAddress, items);
 
         await _orderRepository.AddAsync(order);
+        //await NotifyDeliveryService(order);
+    }
+
+    public async Task NotifyDeliveryService(Order order)
+    {
+        var _httpClient = new HttpClient();
+
+        var orderDetails = new
+        {
+            ShippingAddress = order.ShipToAddress.ToString(),
+            Items = order.OrderItems.Select(i => new { i.ItemOrdered.ProductName, i.Units, i.UnitPrice }),
+            FinalPrice = order.OrderItems.Sum(i => i.Units * i.UnitPrice )
+        };
+
+        var response = await _httpClient.PostAsJsonAsync("http://localhost:7038/api/SaveOrderDetails", orderDetails);
+
+        if (!response.IsSuccessStatusCode)
+        {
+            _logger.LogError("Failed to send order details to delivery service: {StatusCode}", response.StatusCode);
+        }
     }
 }
